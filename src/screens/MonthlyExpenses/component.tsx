bb305d62014fa0ea0a2ea30b8ba940a626455345
Tiago Ribeiro <tiago@accountable.com>
2020-06-01 17:43:17 +0200

    handle total left update on item toggle

--- src/screens/MonthlyExpenses/component.tsx
+++ src/screens/MonthlyExpenses/component.tsx
@@ -7,14 +7,24 @@ import { Item } from '../../types';
 interface Props {
   items: Item[];
   togglePaidStatus: (id: string) => void;
+  updateAmountLeft: (amount: number, isPaid: boolean) => void;
 }
 
-export default function MonthlyExpenses({ items, togglePaidStatus }: Props) {
+export default function MonthlyExpenses({
+  items,
+  togglePaidStatus,
+  updateAmountLeft,
+}: Props) {
+  const onItemPress = ({ id, isPaid, amount }: Item) => {
+    togglePaidStatus(id);
+    updateAmountLeft(amount, isPaid);
+  };
+
   return (
     <View style={styles.container}>
       <TopBar />
 
-      <List data={items} onItemPress={togglePaidStatus} />
+      <List data={items} onItemPress={onItemPress} />
     </View>
   );
 }
