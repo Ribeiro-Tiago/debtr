7f24b300b43a671bbd05ac3adfdc41ef2126d147
Tiago Ribeiro <tiago@accountable.com>
2020-06-01 17:17:37 +0200

    added component > store connection

---
+++ src/screens/MonthlyExpenses/index.ts
@@ -0,0 +1,26 @@
+import { connect } from 'react-redux';
+
+import component from './component';
+import { StoreState } from '../../types/store';
+import { Item } from '../../types';
+import { toggleItemStatus } from '../../store/actions/items';
+
+const getMonthlyItems = (items: Item[]) => {
+  const currMonth = new Date().getMonth();
+
+  return items.filter(({ months }) => months.includes(currMonth));
+};
+
+const mapStateToProps = (state: StoreState) => {
+  return {
+    items: getMonthlyItems(state.items),
+  };
+};
+
+const mapDispatchToProps = (dispatch: Function) => {
+  return {
+    togglePaidStatus: (id: string) => dispatch(toggleItemStatus(id)),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(component);
