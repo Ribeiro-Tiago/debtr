ed80d22908221a2d045368f058b9672bba3da9c7
Tiago Ribeiro <tiago@accountable.com>
2020-06-01 18:39:25 +0200

    made topBar content dynamic

--- src/components/TopBar/component.tsx
+++ src/components/TopBar/component.tsx
@@ -1,19 +1,9 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import { getStatusBarHeight } from 'react-native-status-bar-height';
-import dayjs from 'dayjs';
 
-interface Props {
-  amountLeft: number;
-}
-
-export default function TopBar({ amountLeft }: Props) {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>{dayjs().format('MMMM YYYY')}</Text>
-      <Text style={styles.leftover}>{amountLeft}€ Left</Text>
-    </View>
-  );
+export default function TopBar({ children }) {
+  return <View style={styles.container}>{children}</View>;
 }
 
 const styles = StyleSheet.create({
@@ -32,14 +22,5 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     paddingLeft: 15,
     paddingTop: getStatusBarHeight(),
-  },
-  title: {
-    fontWeight: 'bold',
-    fontSize: 20,
-    color: '#581c0c',
-  },
-  leftover: {
-    fontSize: 16,
-    color: '#581c0c',
   },
 });
