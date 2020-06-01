7e71d4d499974c928eaadc33f73f0ceaedc81372
Tiago Ribeiro <tiago@accountable.com>
2020-06-01 18:55:28 +0200

    replaced bottom bar with top bar to have swipe animation

--- src/screens/Navigator/component.tsx
+++ src/screens/Navigator/component.tsx
@@ -1,6 +1,9 @@
 import React, { useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {
+  createMaterialTopTabNavigator,
+  MaterialTopTabBarOptions,
+} from '@react-navigation/material-top-tabs';
 
 import { AllExpensesScreen, MonthlyExpensesScreen } from '../../screens';
 import { getItems } from '../../services/storage/storage';
@@ -11,7 +14,23 @@ interface Props {
   setItems: (items: Item[]) => void;
 }
 
-const Tab = createBottomTabNavigator();
+const Tab = createMaterialTopTabNavigator();
+
+const barOptions: MaterialTopTabBarOptions = {
+  activeTintColor: '#581c0c',
+  pressOpacity: 0.8,
+  allowFontScaling: true,
+  bounces: true,
+  style: {
+    backgroundColor: '#f1e3cb',
+    borderTopColor: '#a6a6a6',
+    borderTopWidth: 1,
+  },
+  indicatorStyle: {
+    borderBottomColor: '#581c0c',
+    backgroundColor: '#581c0c',
+  },
+};
 
 export default function ({ setAmountLeft, setItems }: Props) {
   // load stuff from local storage
@@ -33,7 +52,7 @@ export default function ({ setAmountLeft, setItems }: Props) {
 
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator tabBarOptions={barOptions} tabBarPosition="bottom">
         <Tab.Screen name="Monthly expenses" component={MonthlyExpensesScreen} />
         <Tab.Screen name="All expenses" component={AllExpensesScreen} />
       </Tab.Navigator>
