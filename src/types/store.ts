f2eaf5a7b5c1083ccbd3ba97c15bdfcc39a80afd
Tiago Ribeiro <tiago@accountable.com>
2020-06-01 17:17:28 +0200

    added interface for store state

--- src/types/store.ts
+++ src/types/store.ts
@@ -1,5 +1,10 @@
 import { Item } from './';
 
+export interface StoreState {
+  items: Item[];
+  amountLeft: number;
+}
+
 export interface ListReducerAction {
   type: string;
   payload: string & Item;
