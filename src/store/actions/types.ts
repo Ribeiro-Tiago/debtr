d56b4839ef221e81c925c2be575fa37ba8c78058
Tiago Ribeiro <tiago@accountable.com>
2020-06-01 18:33:40 +0200

    added functions to set items nad amount left on startup

--- src/store/actions/types.ts
+++ src/store/actions/types.ts
@@ -5,3 +5,4 @@ export const TOGGLE_ITEM_STATUS = 'toggle item status';
 export const ADD_ITEM = 'add item';
 export const REMOVE_ITEM = 'remove item';
 export const UPDATE_ITEM = 'update item';
+export const SET_ITEMS = 'set items';
