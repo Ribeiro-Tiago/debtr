ac7f09678e5c374eb6a5ccc8d23e5891ea68e03a
Tiago Ribeiro <tiago@accountable.com>
2020-06-01 18:33:07 +0200

    added react native storage

--- src/services/storage/storage.ts
+++ src/services/storage/storage.ts
@@ -1,13 +1,13 @@
-import { AsyncStorage } from 'react-native';
+import storage from '@react-native-community/async-storage';
 
 import { StorageData } from '../../types';
 
 const key = 'MPT_DATA';
 
 export const getItems = async () => {
-  JSON.parse(await AsyncStorage.getItem(key)) as StorageData;
+  return JSON.parse(await storage.getItem(key)) as StorageData;
 };
 
 export const saveItems = async (data: StorageData) => {
-  return await AsyncStorage.setItem(key, JSON.stringify(data));
+  return await storage.setItem(key, JSON.stringify(data));
 };
