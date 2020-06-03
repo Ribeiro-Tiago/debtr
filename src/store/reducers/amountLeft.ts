6a04437d5bdeeea0ef9d10adeaec3557beec1847
Tiago Ribeiro <tiago@accountable.com>
2020-06-03 19:11:47 +0200

    added functionality to update storage items and amountleft on store change

--- src/store/reducers/amountLeft.ts
+++ src/store/reducers/amountLeft.ts
@@ -2,7 +2,9 @@ import {
   SUBTRACT_AMOUNT_LEFT,
   ADD_AMOUNT_LEFT,
   SET_AMOUNT_LEFT,
+} from '../actions/types';
 import { AmountLeftReducerAction } from '../../types/store';
+import { updateAmount } from '../../services/storage/storage';
 
 const initState = 0;
 
@@ -16,11 +18,19 @@ export default (
     }
 
     case ADD_AMOUNT_LEFT: {
-      return Number(state) + Number(payload);
+      const amount = Number(state) + Number(payload);
+
+      updateAmount(amount);
+
+      return amount;
     }
 
     case SUBTRACT_AMOUNT_LEFT: {
-      return Number(state) - Number(payload);
+      const amount = Number(state) - Number(payload);
+
+      updateAmount(amount);
+
+      return amount;
     }
 
     default:
