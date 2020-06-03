976f6ad3500955ccf493d97165f8520351ffca95
Tiago Ribeiro <tiago@accountable.com>
2020-06-03 18:29:33 +0200

    added item delete from all expenses

--- src/screens/AllExpenses/index.ts
+++ src/screens/AllExpenses/index.ts
@@ -4,11 +4,13 @@ import component from './component';
 import { StoreState } from '../../types/store';
 import { Item } from '../../types';
 import { updateSelected } from '../../store/actions/current';
+import { removeItem } from '../../store/actions/items';
 
 const mapStateToProps = (state: StoreState) => ({ items: state.items });
 
 const mapDispatchToProps = (dispatch: Function) => ({
   updateCurrent: (item?: Item) => dispatch(updateSelected(item)),
+  removeItem: (id: string) => dispatch(removeItem(id)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(component);
