8b9cf17dbbe74b80cb9da05bb6253766fecf17ab
Tiago Ribeiro <tiago@accountable.com>
2020-06-03 18:58:39 +0200

    updated storage service

--- src/services/storage/storage.ts
+++ src/services/storage/storage.ts
@@ -1,13 +1,35 @@
 import storage from '@react-native-community/async-storage';
 
-import { StorageData } from '../../types';
+import { StorageData, Item } from '../../types';
 
 const key = 'MPT_DATA';
 
-export const getItems = async () => {
+const get = async () => {
   return JSON.parse(await storage.getItem(key)) as StorageData;
 };
 
-export const saveItems = async (data: StorageData) => {
+const set = async (data: StorageData) => {
   return await storage.setItem(key, JSON.stringify(data));
+};
+
+export const getData = async () => get();
+
+export const updateCurrMonth = async (currMonth: number) => {
+  const data = await get();
+
+  return !data
+    ? set({ amountLeft: 0, items: [], currMonth })
+    : set({ ...data, currMonth });
+};
+
+export const updateAmount = async (amount: number) => {
+  const data = await get();
+
+  set({ ...data, amountLeft: Number(data.amountLeft) + amount });
+};
+
+export const updateItems = async (items: Item[]) => {
+  const data = await get();
+
+  set({ ...data, items });
 };
