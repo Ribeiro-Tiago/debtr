e6bba25225e13d8e56605cc868227058b1623694
Tiago Ribeiro <tiago@accountable.com>
2020-06-04 16:05:35 +0200

    deploy stufsf

--- android/app/src/main/java/com/monthlypaytracker/MainActivity.java
+++ android/app/src/main/java/com/monthlypaytracker/MainActivity.java
@@ -10,7 +10,7 @@ import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 public class MainActivity extends ReactActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        SplashScreen.show(this, R.style.SplashScreenTheme);
+        SplashScreen.show(this);
         super.onCreate(savedInstanceState);
     }
     /**
