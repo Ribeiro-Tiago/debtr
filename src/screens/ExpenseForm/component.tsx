44cd66e55841e45d675bfdad553fe2718c579a34
Tiago Ribeiro <tiago@accountable.com>
2020-06-05 16:01:53 +0200

    minor fixes

--- src/screens/ExpenseForm/component.tsx
+++ src/screens/ExpenseForm/component.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
 import {
   View,
   StyleSheet,
@@ -8,13 +8,13 @@ import {
   ScrollView,
   Alert,
   KeyboardAvoidingView,
-} from 'react-native';
-import { useForm } from 'react-hook-form';
-import { useNavigation } from '@react-navigation/native';
-import Icon from 'react-native-vector-icons/Ionicons';
+} from "react-native";
+import { useForm } from "react-hook-form";
+import { useNavigation } from "@react-navigation/native";
+import Icon from "react-native-vector-icons/Ionicons";
 
-import { Item, Month, ItemCreation } from '../../types';
-import { TopBar, FormItem, MonthSelector } from '../../components';
+import { Item, Month, ItemCreation } from "../../types";
+import { TopBar, FormItem, MonthSelector } from "../../components";
 
 interface Props {
   item: Item | null;
@@ -43,13 +43,13 @@ export default function ExpenseForm({
   const { goBack } = useNavigation();
   const { register, setValue, handleSubmit, errors } = useForm<Form>({
     defaultValues: initialValues,
-    reValidateMode: 'onBlur',
+    reValidateMode: "onBlur",
   });
   const isNew = !item;
 
   useEffect(() => {
-    register({ name: 'description' }, { required: true });
-    register({ name: 'amount' }, { required: true });
+    register({ name: "description" }, { required: true });
+    register({ name: "amount" }, { required: true });
   }, [register]);
 
   const onSubmit = (data: Form) => {
@@ -58,7 +58,7 @@ export default function ExpenseForm({
     } else {
       update(
         { ...item, ...data, months: selectedMonths },
-        initialValues.amount
+        initialValues.amount,
       );
     }
 
@@ -72,10 +72,10 @@ export default function ExpenseForm({
 
   const onDelete = () => {
     Alert.alert(
-      'Confirm delete?',
-      'You are about to delete this expense. This action is irreversable',
-      [{ text: 'Confirm', onPress: onDeleteConfirm }, { text: 'Cancel' }],
-      { cancelable: true }
+      "Confirm delete?",
+      "You are about to delete this expense. This action is irreversable",
+      [{ text: "Confirm", onPress: onDeleteConfirm }, { text: "Cancel" }],
+      { cancelable: true },
     );
   };
 
@@ -86,7 +86,7 @@ export default function ExpenseForm({
           <TouchableWithoutFeedback onPress={goBack}>
             <View style={styles.goBackWrapper}>
               <Icon
-                name={`${Platform.OS === 'ios' ? 'ios' : 'md'}-arrow-back`}
+                name={`${Platform.OS === "ios" ? "ios" : "md"}-arrow-back`}
                 color="#581c0c"
                 size={24}
               />
@@ -95,7 +95,7 @@ export default function ExpenseForm({
           </TouchableWithoutFeedback>
 
           <Text style={styles.title}>
-            {isNew ? 'New expense' : 'Updating expense'}
+            {isNew ? "New expense" : "Updating expense"}
           </Text>
         </View>
       </TopBar>
@@ -121,18 +121,17 @@ export default function ExpenseForm({
   const renderForm = () => {
     return (
       <KeyboardAvoidingView
-        style={{ flex: 1, padding: 20 }}
-        behavior={Platform.OS == 'ios' ? 'padding' : 'height'}
+        style={styles.container}
+        behavior={Platform.OS == "ios" ? "padding" : "height"}
         enabled
-        keyboardVerticalOffset={0}
-      >
-        <ScrollView style={{ flex: 1 }}>
+        keyboardVerticalOffset={0}>
+        <ScrollView style={styles.wrapper}>
           <FormItem
             label="Description"
             error="Description is required"
             placeholder="E.g.: Netflix"
             hasErr={!!errors.description}
-            onChange={(val: string) => setValue('description', val, true)}
+            onChange={(val: string) => setValue("description", val, true)}
             initialValue={item && item.description}
           />
 
@@ -142,7 +141,7 @@ export default function ExpenseForm({
             placeholder="E.g.: 14 (Just a number)"
             hasErr={!!errors.amount}
             keyboard="numeric"
-            onChange={(val: number) => setValue('amount', val, true)}
+            onChange={(val: string) => setValue("amount", Number(val), true)}
             initialValue={item && item.amount.toString()}
           />
 
@@ -155,7 +154,7 @@ export default function ExpenseForm({
   };
 
   return (
-    <View style={{ flex: 1 }}>
+    <View style={styles.wrapper}>
       {renderTopBar()}
 
       {renderForm()}
@@ -164,48 +163,54 @@ export default function ExpenseForm({
 }
 
 const styles = StyleSheet.create({
+  wrapper: {
+    flex: 1,
+  },
+  container: {
+    flex: 1,
+    paddingHorizontal: 20,
+  },
   buttonDelete: {
-    color: 'red',
+    color: "red",
     marginTop: 30,
     fontSize: 16,
   },
   buttonSubmit: {
-    color: '#ca5116',
+    color: "#ca5116",
     borderWidth: 1,
-    borderColor: '#ca5116',
+    borderColor: "#ca5116",
     borderRadius: 10,
     fontSize: 18,
-    width: '70%',
-    textAlign: 'center',
+    width: "70%",
+    textAlign: "center",
     paddingVertical: 10,
   },
   buttonWrapper: {
-    marginTop: 30,
-    marginBottom: 50,
-    display: 'flex',
-    alignItems: 'center',
+    marginVertical: 30,
+    display: "flex",
+    alignItems: "center",
   },
   topbarContainer: {
     flex: 1,
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
   },
   goBackWrapper: {
-    display: 'flex',
-    alignItems: 'center',
-    flexDirection: 'row',
-    position: 'absolute',
+    display: "flex",
+    alignItems: "center",
+    flexDirection: "row",
+    position: "absolute",
     left: 0,
   },
   goBackText: {
     fontSize: 18,
     marginLeft: 10,
-    color: '#581c0c',
+    color: "#581c0c",
   },
   title: {
     fontSize: 18,
-    color: '#581c0c',
-    textAlign: 'center',
+    color: "#581c0c",
+    textAlign: "center",
   },
 });
