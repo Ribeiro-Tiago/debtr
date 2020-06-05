c6b66824b43f0173a5e26c2f276c1cf384f7c784
Tiago Ribeiro <tiago@accountable.com>
2020-06-05 14:36:31 +0200

    updated app name

--- android/app/src/main/java/com/debtr/MainActivity.java
+++ android/app/src/main/java/com/debtr/MainActivity.java
@@ -1,15 +1,21 @@
 package com.debtr;
 
+import android.os.Bundle;
+import org.devio.rn.splashscreen.SplashScreen;
 import com.facebook.react.ReactActivity;
 
 public class MainActivity extends ReactActivity {
-
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen.show(this, R.style.SplashScreenTheme);
+        super.onCreate(savedInstanceState);
+    }
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
   @Override
   protected String getMainComponentName() {
-    return "debtr";
+    return "Debtr - Your payment tracker assistant";
   }
 }
