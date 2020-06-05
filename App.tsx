518a694acb7157a382ebc7baa7892e20d9ad55e8
Tiago Ribeiro <tiago@accountable.com>
2020-06-05 14:08:24 +0200

    update entire structure to remove expo

--- App.tsx
+++ App.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import { Provider } from 'react-redux';
+import React from "react";
+import { Provider } from "react-redux";
 
-import store from './src/store';
-import Navigator from './src/screens/Navigator';
+import store from "./src/store";
+import Navigator from "./src/screens/Navigator";
 
 export default function App() {
   return (
