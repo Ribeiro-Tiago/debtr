1eada786d39fccb714f97b9ee40b5d287094365d
Tiago Ribeiro <tiago@accountable.com>
2020-06-10 12:27:41 +0200

    added validation to only update amountleft is item is on current month

--- src/screens/AllExpenses/index.ts
+++ src/screens/AllExpenses/index.ts
@@ -1,19 +1,23 @@
-import { connect } from 'react-redux';
+import { connect } from "react-redux";
 
-import component from './component';
-import { StoreState } from '../../types/store';
-import { Item } from '../../types';
-import { updateSelected } from '../../store/actions/current';
-import { removeItem } from '../../store/actions/items';
-import { subtractAmount } from '../../store/actions/amountLeft';
+import component from "./component";
+import { StoreState } from "../../types/store";
+import { Item, Month } from "../../types";
+import { updateSelected } from "../../store/actions/current";
+import { removeItem } from "../../store/actions/items";
+import { subtractAmount } from "../../store/actions/amountLeft";
+import { isCurrentMonth } from "../../utils";
 
 const mapStateToProps = (state: StoreState) => ({ items: state.items });
 
 const mapDispatchToProps = (dispatch: Function) => ({
   updateCurrent: (item?: Item) => dispatch(updateSelected(item)),
-  removeItem: (id: string, amount: number) => {
+  removeItem: (id: string, months: Month[], amount: number) => {
     dispatch(removeItem(id));
-    dispatch(subtractAmount(amount));
+
+    if (isCurrentMonth(months)) {
+      dispatch(subtractAmount(amount));
+    }
   },
 });
 
