388a12a83a6be6def22c65b96cce38ad826ac8fe
Tiago Ribeiro <tiagoribeiro.1803@gmail.com>
2020-06-18 17:57:37 +0200

    added navigatro & selector screen on index

--- src/screens/index.ts
+++ src/screens/index.ts
@@ -1,5 +1,13 @@
-import AllExpensesScreen from './AllExpenses';
-import MonthlyExpensesScreen from './MonthlyExpenses';
-import ExpenseFormScreen from './ExpenseForm';
+import AllExpensesScreen from "./AllExpenses";
+import MonthlyExpensesScreen from "./MonthlyExpenses";
+import ExpenseFormScreen from "./ExpenseForm";
+import Navigator from "./Navigator";
+import LocaleSelectorScreen from "./LocaleSelector";
 
-export { AllExpensesScreen, MonthlyExpensesScreen, ExpenseFormScreen };
+export {
+  AllExpensesScreen,
+  MonthlyExpensesScreen,
+  ExpenseFormScreen,
+  Navigator,
+  LocaleSelectorScreen,
+};
