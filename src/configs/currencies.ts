f2a24461f21b53b78f8196120381ff7e3633b856
Tiago Ribeiro <tiagoribeiro.1803@gmail.com>
2020-06-19 19:43:05 +0200

    fixed typos

--- src/configs/currencies.ts
+++ src/configs/currencies.ts
@@ -2,6 +2,6 @@ import { SupportedCurrencies } from "../types";
 
 export const currencies = [
   { key: SupportedCurrencies.EUR, label: "Euro (€)" },
-  { key: SupportedCurrencies.USD, label: "Untied States Dollar ($)" },
-  { key: SupportedCurrencies.EUR, label: "Great British Poud (£)" },
+  { key: SupportedCurrencies.USD, label: "United States Dollar ($)" },
+  { key: SupportedCurrencies.GDP, label: "Great British Poud (£)" },
 ];
