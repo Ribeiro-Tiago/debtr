8751e4466a7665f0bff60a53e166aafe5dfc6faa
Tiago Ribeiro <tiagoribeiro.1803@gmail.com>
2020-06-21 19:28:01 +0200

    fixed android related issues

--- android/app/src/main/java/com/debtr/MainApplication.java
+++ android/app/src/main/java/com/debtr/MainApplication.java
@@ -27,11 +27,6 @@ public class MainApplication extends Application implements ReactApplication {
         protected List<ReactPackage> getPackages() {
           @SuppressWarnings("UnnecessaryLocalVariable")
           List<ReactPackage> packages = new PackageList(this).getPackages();
-
-          packages.add(new ReactNativePushNotificationPackage());
-
-          // Packages that cannot be autolinked yet can be added manually here, for example:
-          /* packages.add(new SomePackage()); */
           return packages;
         }
 
