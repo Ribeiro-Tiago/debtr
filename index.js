c7f6548f5cd7adb1fe5c8efc0b4564439a6e2dfa
Tiago Ribeiro <tiagoribeiro.1803@gmail.com>
2020-06-21 11:36:18 +0200

    updated app name for android and ios

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-import { AppRegistry } from "react-native";
-import { YellowBox } from "react-native";
+import { AppRegistry, YellowBox, Platform } from "react-native";
 
 import App from "./App";
-import { name as appName } from "./app.json";
+import { iosName, androidName } from "./app.json";
 
 // temporary until react-native-picker-select merges PR#303
 YellowBox.ignoreWarnings(["Picker has been extracted"]);
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(
+  Platform.OS === "ios" ? iosName : androidName,
+  () => App,
+);
