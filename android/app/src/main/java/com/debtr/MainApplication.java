bd1b99b7593c0286a391f285a877624e8a4c5ff6
Tiago Ribeiro <tiagoribeiro.1803@gmail.com>
2020-06-21 12:30:40 +0200

    installed dependecy and configured stuff

--- android/app/src/main/java/com/debtr/MainApplication.java
+++ android/app/src/main/java/com/debtr/MainApplication.java
@@ -12,6 +12,7 @@ import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 import org.devio.rn.splashscreen.SplashScreenReactPackage;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -26,6 +27,9 @@ public class MainApplication extends Application implements ReactApplication {
         protected List<ReactPackage> getPackages() {
           @SuppressWarnings("UnnecessaryLocalVariable")
           List<ReactPackage> packages = new PackageList(this).getPackages();
+
+          packages.add(new ReactNativePushNotificationPackage());
+
           // Packages that cannot be autolinked yet can be added manually here, for example:
           /* packages.add(new SomePackage()); */
           return packages;
