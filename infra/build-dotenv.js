b19f7fd881569a090627e490df325d0a15a35918
Tiago Ribeiro <tiagoribeiro.1803@gmail.com>
2020-09-09 12:26:21 +0200

    infra: add script to build dotenv from secrets

---
+++ infra/build-dotenv.js
@@ -0,0 +1,19 @@
+const setup = () => {
+  const envVars = Object.entries(process.env).filter(([key]) => {
+    return key.includes("DOTENV_");
+  });
+
+  if (!envVars.length) {
+    return;
+  }
+
+  const { writeFileSync } = require("fs");
+  const { resolve } = require("path");
+
+  writeFileSync(
+    resolve(__dirname, "../.env"),
+    envVars.map(([key, value]) => `${key}=${value}`).join("\n"),
+  );
+};
+
+setup();
